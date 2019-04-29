1948afe5f2d92618e202e6616ff9f3599f6f9fab
Wang <yimei.wym@gmail.com>
2019-04-29 13:31:00 +0800

    Connected database

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
+import peeweedbevolve
 from flask import Flask, render_template, request
+from models import db
+
 app = Flask(__name__)
+
+
+@app.before_request
+def before_request():
+    db.connect()
+
+
+@app.after_request
+def after_request(response):
+    db.close()
+    return response
 
 
 @app.route("/")
@@ -7,5 +21,5 @@ def index():
     return render_template("index.html")
 
 
-if __name == 'main':
+if __name__ == 'main':
     app.run()
