1229398c94a00f0bbf2149053802401e840a8c66
Wang <yimei.wym@gmail.com>
2019-04-29 13:17:51 +0800

    Added template files

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name == 'main':
+    app.run()
